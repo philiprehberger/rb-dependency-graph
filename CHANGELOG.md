0c1f1878788b4d136787e75134144ae82fe3ba8e
Reverend Philip <me@philiprehberger.com>
2017-04-13 00:11:19 -0600

    Release v0.1.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,7 +6,13 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
-n## [0.1.2] - 2026-03-22
+
+## [0.1.3] - 2026-03-22
+
+### Added
+- Expand test coverage to 30+ examples with edge cases for empty graphs, single nodes, disconnected components, self-cycles, long chains, string/integer keys, and chaining
+
+## [0.1.2] - 2026-03-22
 
 ### Changed
 - Version bump for republishing
--- spec/philiprehberger/dependency_graph_spec.rb
+++ spec/philiprehberger/dependency_graph_spec.rb
@@ -171,5 +171,105 @@ RSpec.describe Philiprehberger::DependencyGraph do
         expect(cycles.first.size).to be >= 2
       end
     end
+
+    describe 'edge cases' do
+      it 'handles an empty graph' do
+        expect(graph.nodes).to be_empty
+        expect(graph.resolve).to eq([])
+        expect(graph.cycle?).to be false
+        expect(graph.cycles).to be_empty
+      end
+
+      it 'handles a single node with no dependencies' do
+        graph.add(:only)
+        expect(graph.resolve).to eq([:only])
+        expect(graph.parallel_batches).to eq([[:only]])
+      end
+
+      it 'handles a single node depending on itself (self-cycle)' do
+        graph.add(:a, depends_on: [:a])
+        expect(graph.cycle?).to be true
+      end
+
+      it 'handles adding the same node multiple times without dependencies' do
+        graph.add(:a)
+        graph.add(:a)
+        expect(graph.nodes.keys.count(:a)).to eq(1)
+      end
+
+      it 'handles adding multiple dependencies in one call' do
+        graph.add(:d, depends_on: %i[a b c])
+        expect(graph.nodes[:d]).to contain_exactly(:a, :b, :c)
+      end
+
+      it 'resolves disconnected components' do
+        graph.add(:a)
+        graph.add(:b, depends_on: [:a])
+        graph.add(:x)
+        graph.add(:y, depends_on: [:x])
+
+        result = graph.resolve
+        expect(result.index(:a)).to be < result.index(:b)
+        expect(result.index(:x)).to be < result.index(:y)
+        expect(result.size).to eq(4)
+      end
+
+      it 'produces correct parallel batches for disconnected components' do
+        graph.add(:a)
+        graph.add(:b, depends_on: [:a])
+        graph.add(:x)
+        graph.add(:y, depends_on: [:x])
+
+        batches = graph.parallel_batches
+        expect(batches.first).to contain_exactly(:a, :x)
+        expect(batches[1]).to contain_exactly(:b, :y)
+      end
+
+      it 'handles long chains' do
+        graph.add(:a)
+        graph.add(:b, depends_on: [:a])
+        graph.add(:c, depends_on: [:b])
+        graph.add(:d, depends_on: [:c])
+        graph.add(:e, depends_on: [:d])
+
+        result = graph.resolve
+        expect(result).to eq(%i[a b c d e])
+      end
+
+      it 'handles string keys' do
+        graph.add('web', depends_on: ['db'])
+        graph.add('db')
+
+        result = graph.resolve
+        expect(result.index('db')).to be < result.index('web')
+      end
+
+      it 'handles integer keys' do
+        graph.add(1)
+        graph.add(2, depends_on: [1])
+
+        result = graph.resolve
+        expect(result).to eq([1, 2])
+      end
+
+      it 'detects a three-node cycle' do
+        graph.add(:a, depends_on: [:b])
+        graph.add(:b, depends_on: [:c])
+        graph.add(:c, depends_on: [:a])
+
+        expect(graph.cycle?).to be true
+        expect { graph.resolve }.to raise_error(described_class::Error, /Cycle detected/)
+      end
+
+      it 'parallel_batches returns empty for empty graph' do
+        expect(graph.parallel_batches).to eq([])
+      end
+
+      it 'supports chaining add calls' do
+        graph.add(:a).add(:b, depends_on: [:a]).add(:c, depends_on: [:b])
+        result = graph.resolve
+        expect(result).to eq(%i[a b c])
+      end
+    end
   end
 end
